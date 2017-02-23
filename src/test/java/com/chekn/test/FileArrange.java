b43194af16d3ac029bac080e4add11c98dcdb50c
chekn <chekn@3FCD3B5572-PC>
2017-02-23 08:27:48 +0800

    change log implements

--- src/test/java/com/chekn/test/FileArrange.java
+++ src/test/java/com/chekn/test/FileArrange.java
@@ -8,10 +8,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class FileArrange {
 	public static final int FULLFILL=0;
 	public static final int DELETE=1;
-	
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	public void core(String path, int aeType) throws Exception{
 		
@@ -28,7 +33,7 @@ public class FileArrange {
 			}else{
 				if(files[i].getName().equals("Thumbs.db")) continue;
 				
-				System.out.println("当前处理文件："+files[i].getName());
+				logger.info("当前处理文件："+files[i].getName());
 				
 				//this.writeStr(files[i], UUID.randomUUID().toString());
 				if(aeType==FileArrange.FULLFILL){
@@ -54,7 +59,7 @@ public class FileArrange {
 			fw.flush();
 			fw.close();
 		} catch (IOException e) {
-			System.err.println(a.getName()+"操作失败");
+			logger.error(a.getName()+"操作失败");
 		}
 		
 	}
@@ -75,7 +80,7 @@ public class FileArrange {
 			dos.flush();
 			dos.close();
 		} catch (IOException e) {
-			System.err.println(a.getName()+"操作失败");
+			logger.error(a.getName()+"操作失败");
 		}
 	}
 	
